9edf242dd9b3ec626fecf82281a1499512eb79ae
Ramon <shaobig@gmail.com>
2025-10-30 13:12:32 +0200

    alpha/1.2.1: Change rotation depending on the ammo velocity vector

--- Assets/Scripts/Weapon/Ammo/AmmoController.cs
+++ Assets/Scripts/Weapon/Ammo/AmmoController.cs
@@ -4,11 +4,18 @@ public class AmmoController : MonoBehaviour
 {
     [SerializeField]
     private GameObjectRemover gameObjectRemover;
+    private new Rigidbody rigidbody;
     private OnAmmoCollideWithTerrainListener onAmmoCollideWithTerrainListener;
 
     public void Init(OnAmmoCollideWithTerrainListener onAmmoCollideWithTerrainListener)
     {
         this.onAmmoCollideWithTerrainListener = onAmmoCollideWithTerrainListener;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation(rigidbody.linearVelocity);
     }
 
     void OnCollisionEnter(Collision collision)
